3b51448b061132167555597bba9c66a7bc121bf0
Wiktor <wiktor.wojcikowski@gmail.com>
2016-04-08 14:16:19 +0200

    interactive map tooltips

--- src/chart-types/interactive-map.js
+++ src/chart-types/interactive-map.js
@@ -71,12 +71,22 @@
         center: metadata.geo_center,
         zoom: radiusToZoom(metadata.geo_radius)
     });
+    
+    var tooltip = d3.select(element)
+      .append("div")
+      .classed("popily-tooltip-container", true)
+      .style("position", "fixed")
+      .style("top", "0px")
+      .style("visibility", "hidden")
+
+    
     var scale = d3.scale.linear();
     if(amounts.length > 0) {
       amounts = _.map(amounts, function(a) {return parseFloat(a);});
       scale.domain([_.min(amounts), _.max(amounts)]);
     }
     scale.range([metadata.geo_radius * 1, metadata.geo_radius * 250]);
+    var showTooltip = (_.isUndefined(options.tooltip)?true:options.tooltip);
 
     if(analysisType.indexOf('_category') > -1) {
         var categoryColorMap = {};
@@ -100,7 +110,7 @@
         _.each(uniqueCategories, function(uc) {
             markerMap[uc] = [];
         });
-
+        
         _.each(_.zip(coords,categories,labels,amounts), function(coord) {
             var size = 20;
             if(amounts.length > 0) {
@@ -111,15 +121,33 @@
                 color: categoryColorMap[coord[1]],
                 fillColor: categoryColorMap[coord[1]],
                 fillOpacity: 0.5
-            }).addTo(mapObj);
-
-            if(labels.length == coords.length) {
-                marker.bindPopup(coord[2]);
+            })
+            if(showTooltip) {
+              marker
+                .on('mouseover', function(e) { 
+                  var rowLabel = (labels.length == coords.length ? coord[2] : coord[1]);
+                  var markup = '<table class="popily-tooltip"><tbody>';
+                  markup += '<tr><th colspan="2"><span style="background: '+categoryColorMap[coord[1]]+'"></span>'+coord[1]+'</th></tr>';
+                  
+                  if(!_.isUndefined(coord[3]))
+                    markup += '<tr class="popily-tooltip-name"><td class="name">'+rowLabel+'</td><td class="value">'+coord[3]+'</td></tr>';
+                  else
+                    markup += '<tr class="popily-tooltip-name"><td class="name" colspan="2">'+rowLabel+'</td></tr>';
+                  markup += '</tbody></table>'
+                  
+                  tooltip
+                    .style("top", (e.originalEvent.y-10)+"px")
+                    .style("left",(e.originalEvent.x+10)+"px")
+                    .html(markup)
+                    .style("visibility", "visible");
+                })
+                .on('mouseout', function() {
+                  tooltip
+                    .style("visibility", "hidden");
+                })
             }
-            else {
-                marker.bindPopup(coord[1]);
-            }
-
+            
+            marker.addTo(mapObj);
             markerMap[coord[1]].push(marker);
         });
 
@@ -175,20 +203,42 @@
         var color = options.colors[0];
         _.each(_.zip(coords,labels,amounts), function(coord) {
             var size = 20;
-            var popup = coord[1];
             if(amounts.length > 0) {
               size = scale(coord[2]);
-              popup = coord[2];
             }
             var marker = L.circle(coord[0], size, {
                 color: color,
                 fillColor: color,
                 fillOpacity: 0.5
-            }).addTo(mapObj);
-            
-            if(labels.length == coords.length) {
-                marker.bindPopup(popup, {className: 'popily-tooltip'});
+            });
+            if(showTooltip) {
+              marker
+                .on('mouseover', function(e) { 
+                  var rowLabel = coord[1];
+                  var markup = '<table class="popily-tooltip"><tbody>';
+                  
+                  if(!_.isUndefined(coord[2]))
+                    markup += '<tr class="popily-tooltip-name"><td class="name"><span style="background: '+color+'"></span>'+rowLabel+'</td><td class="value">'+coord[2]+'</td></tr>';
+                  else
+                    markup += '<tr class="popily-tooltip-name"><td class="name" colspan="2"><span style="background: '+color+'"></span>'+rowLabel+'</td></tr>';
+                  markup += '</tbody></table>'
+                  
+                  tooltip
+                    .style("top", (e.originalEvent.y-10)+"px")
+                    .style("left",(e.originalEvent.x+10)+"px")
+                    .html(markup)
+                    .style("visibility", "visible");
+                })
+                .on('mouseout', function() {
+                  tooltip
+                    .style("visibility", "hidden");
+                })
             }
+            marker.addTo(mapObj);
+            
+            //if(labels.length == coords.length) {
+            //    marker.bindPopup(popup, {className: 'popily-tooltip'});
+            //}
         });
     }
     mapObj.addLayer(layer);
