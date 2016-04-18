379a7435274a41a7a94ef92852694769a0656b2e
Wiktor <wiktor.wojcikowski@gmail.com>
2016-04-18 23:23:51 +0200

    no default color

--- src/popily.js
+++ src/popily.js
@@ -22,7 +22,7 @@
             '#9FB921', '#0665A2', '#8333A2', '#0F3863', '#BC9805', '#FFE26C', '#E989B6',
             '#C92918', '#FAA96F'
           ],
-          background: "#99F"
+          background: false
       },
       categoryLimit: 500,
       barBubbleCutoff: 30,
